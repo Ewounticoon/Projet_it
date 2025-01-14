58de6f1b8762ab080b022a3caea6a03f688ebc6f
EwenRasbHome <ewengc21@gmail.com>
2025-01-14 20:40:44 +0000

    Save changes before switching branches

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,7 +7,8 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/root/ros_workspace/devel:$CMAKE_PREFIX_PATH"
+export CMAKE_PREFIX_PATH='/root/ros_workspace/devel:/opt/ros/noetic'
+export LD_LIBRARY_PATH='/root/ros_workspace/devel/lib:/opt/ros/noetic/lib'
+export PKG_CONFIG_PATH='/root/ros_workspace/devel/lib/pkgconfig:/opt/ros/noetic/lib/pkgconfig'
 export PWD='/root/ros_workspace/build'
-export ROSLISP_PACKAGE_DIRECTORIES="/root/ros_workspace/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
-export ROS_PACKAGE_PATH="/root/ros_workspace/src:$ROS_PACKAGE_PATH"
+export ROSLISP_PACKAGE_DIRECTORIES='/root/ros_workspace/devel/share/common-lisp'
