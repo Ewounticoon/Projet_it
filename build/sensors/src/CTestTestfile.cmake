8c23ba28bed8638c6dc017cfe350d99b300b1f9d
Rasberry_IT <Rasberry_IT@example.com>
2025-01-14 10:43:20 +0000

    Premieres version noeuds

--- build/sensors/src/CTestTestfile.cmake
+++ build/sensors/src/CTestTestfile.cmake
@@ -1,8 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /root/ros_workspace/src
-# Build directory: /root/ros_workspace/build
+# Source directory: /root/ros_workspace/src/sensors/src
+# Build directory: /root/ros_workspace/build/sensors/src
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("sensors/src")
