71aaf69b7e9f5aa871643f2284b78e019a3ed4b2
Rasberry_IT <Rasberry_IT@example.com>
2025-01-15 10:42:19 +0000

    Ajout micro node

--- src/sensors/src/micro_sensor_node.py
+++ src/sensors/src/micro_sensor_node.py
@@ -2,6 +2,7 @@ import rospy
 from std_msgs.msg import Float32
 from pyfirmata import Arduino, util
 import time
+import math
 
 class Node_micro:
     def __init__(self):
@@ -21,6 +22,12 @@ class Node_micro:
         # Ecrit dans le terminal
         rospy.loginfo("Publication micro")
 
+
+    def voltage_to_db(self, voltage, v_ref=1.0):
+        if voltage > 0 :
+            return 20*math.log10(voltage/v_ref)
+        else :
+            return -float('inf')
     # Publier sur un topic
     def pub_donne_micro(self, event):
         analog_value = self.board.analog[0].read()  # Lire la valeur analogique de la broche A0
@@ -28,8 +35,10 @@ class Node_micro:
         if analog_value is not None:
             # Conversion de la valeur analogique en tension (de 0 à 5V)
             voltage = analog_value * 5.0
+            db_value=self.voltage_to_db(voltage,1)
+            #rospy.loginfo("Analog Value : %f, Voltage : %f, dB Value : %f" , analog_value, voltage, db_value)
             msg = Float32()
-            msg.data = voltage  # Assigner la tension calculée au message
+            msg.data = db_value  # Assigner la tension calculée au message
             self.topic_micro.publish(msg)  # Publier le message
 
 # Démarrer le noeud
