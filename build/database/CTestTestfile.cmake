9a69e2dae8a49aea533370eefcdc4e0c052d0045
Ewen_GC <ewengc21@gmail.com>
2025-01-15 09:23:53 +0100

    Ajout .gitignore

--- build/database/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/ros_workspace/src/database
-# Build directory: /root/ros_workspace/build/database
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
