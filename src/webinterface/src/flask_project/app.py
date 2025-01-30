27eab4a892ca35144c58fd03f93ebbe660ab2eae
Enzo-B-25 <enzo.boissenin@gmail.com>
2025-01-30 09:14:35 +0100

    Résolution des conflits entre develop et webinterface

--- src/webinterface/src/flask_project/app.py
+++ src/webinterface/src/flask_project/app.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from flask import Flask, render_template, request, redirect, url_for
 import rospy
@@ -7,11 +7,11 @@ from badge_rfid.srv import ajout_badge
 app = Flask(__name__)
 
 # Fonction pour envoyer les informations au service ROS
-def send_user_info(nom, mdp, age, email, password, job_title):
-    rospy.wait_for_service('user_info_service')
+def send_user_info(prenom, nom, age, email, mdp, job_title):
+    rospy.wait_for_service('ajout_badge')
     try:
-        user_info_service = rospy.ServiceProxy('user_info_service', ajout_badge)
-        response = user_info_service(nom, mdp, age, email, password, job_title)
+        user_info_service = rospy.ServiceProxy('ajout_badge', ajout_badge)
+        response = user_info_service(prenom, nom, age, email, mdp, job_title)
         rospy.loginfo(f"Service response: success = {response.success}")
     except rospy.ServiceException as e:
         rospy.logerr(f"Service call failed: {e}")
@@ -27,17 +27,18 @@ def formulaire_badge():
 @app.route('/traitement', methods=['POST'])
 def traitement():
     donnee = request.form
+    prenom = donnee.get('prenom')
     nom = donnee.get('nom')
-    mdp = donnee.get('mdp')
-    age = donnee.get('age')  # Assurez-vous que ces champs existent dans le formulaire
+    age = donnee.get('age')# Assurez-vous que ces champs existent dans le formulaire
     email = donnee.get('email')
+    mdp = donnee.get('mdp')
     job_title = donnee.get('job_title')  # Assurez-vous d'utiliser "job_title" pour correspondre à l'attribut HTML
     
     # Afficher les valeurs reçues
-    print(nom, mdp, age, email, job_title)
+    print(prenom, nom, age, email, mdp, job_title)
 
     # Appeler la fonction d'envoi des données au service ROS
-    send_user_info(nom, mdp, age, email, mdp, job_title)  # Remplace "password123" par le mot de passe réel
+    send_user_info(prenom, nom, int(age), email, mdp, job_title)  # Remplace "password123" par le mot de passe réel
 
     return "Traitement des données effectué", 200
 
