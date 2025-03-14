d723b492412e2a0e7f44b766449b9952801e9e20
PierrotGdt <pierre.gaudet2003@gmail.com>
2025-03-14 15:02:20 +0100

    V1 du code de lecture database user

--- src/badge_rfid/src/nav_database_badge.py
+++ src/badge_rfid/src/nav_database_badge.py
@@ -11,19 +11,22 @@ from badge_rfid.srv import login_member, login_memberReponse
 db_path = os.path.expanduser('~/ros_workspace/src/database/database/RFID_infos.db')
 
 def extract_data(req):
-    username = req.user
+    username = req.username
 
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
     try:
         
         cursor.execute('''
-            SELECT * FROM infos WHERE user = 'username';
+            SELECT mdp, poste FROM infos WHERE user = 'username';
         ''')
-        data_extracted = cursor.fetchall() #stockage des données dans une variable
+        data_extracted = cursor.fetchone() #stockage des données dans une variable
 
-        rospy.loginfo("Donnees extraites avec succes")
-        success = True
+        if data_extracted :
+            password, role = data_extracted #separation des infos dans des varaiables distinctes
+
+            rospy.loginfo("Donnees extraites avec succes")
+            success = True
     except sqlite3.Error as e:
         rospy.logerr(f"Erreur lors de l'extraction des données : {e}")
         success = False
@@ -31,14 +34,16 @@ def extract_data(req):
         conn.commit()
         conn.close()
 
+    return login_memberReponse(sucess,username,password,role) #renvoi les resultats pour le service ROS
 
 
 def listener():
     """Initialise le noeud ROS et les services."""
     rospy.init_node('nav_database', anonymous=True)
 
-    # Définir le service
-    rospy.Service('login_serv', login_member)
+
+    # Définir le service et appel de la fonction de lecture de la databse
+    rospy.Service('login_serv', login_member, extract_data)
 
     rospy.loginfo("Noeud nav_database démarré et en attente de messages...")
     rospy.spin()
